651e864298fec3cceb6d5b72f6c905c2b22d979c
Ryan Chiechi <r.c.chiechi@rug.nl>
2019-03-28 15:41:54 +0100

    FileListBox Movements

--- gui/BJParserFrame.py
+++ gui/BJParserFrame.py
@@ -28,9 +28,10 @@ class BJParserFrame(tk.Frame):
 
     indir=str()
     outdir=str()
-    keep_selected_files = []
-    toss_selected_files = []
-    in_files = []
+    Keep_selected_files = []
+    Toss_selected_files = []
+    Keep_files = []
+    Toss_files = []
     style = ttk.Style()
     boolmap = {1:True, 0:False}
     
@@ -42,19 +43,11 @@ class BJParserFrame(tk.Frame):
         self.master.title("RCCLab STMBJ Data Parser")
         self.master.geometry('900x850+250-250')
         self.pack(fill=tk.BOTH, expand=True)
-        self.update_idletasks() 
-        self.__createWidgets()
-        self.__loadcache()
+        self.update_idletasks()
+        self.__createWidgets()      
         self.ToFront()
         self.mainloop()
-        
-    def __loadcache(self):
-        self.cache = Cache(self.logger)
-        if not self.cache['last_input_path']:
-            self.cache['last_input_path'] = os.getcwd()
-        if not os.path.exists(self.cache['last_input_path']):
-            self.last_input_path = os.path.expanduser('~') 
-        
+
     def __createWidgets(self):
         self.update()
         
@@ -84,6 +77,8 @@ class BJParserFrame(tk.Frame):
         self.logger.setLevel(getattr(logging,self.opts.loglevel.upper()))
         self.logger.info("Logging...")
         self.handler.flush()
+        self.cache = Cache(self.logger)
+        self.checkOptions()
         
 
     def __createButtons(self):
@@ -91,7 +86,8 @@ class BJParserFrame(tk.Frame):
         buttons = [
                {'name':'Quit','text':'QUIT','command':'Quit','side':tk.BOTTOM},
                {'name':'SpawnInputDialog','text':'Add Input Files','side':tk.LEFT},
-               {'name':'RemoveFile','text':'Remove Files','side':tk.LEFT},
+               {'name':'TossFile','text':'Toss Files','side':tk.LEFT},
+               {'name':'KeepFile','text':'Keep Files','side':tk.LEFT},
                {'name':'SpawnOutputDialog','text':'Choose Output Directory','side':tk.LEFT},
                {'name':'Parse','text':'Parse!','side':tk.LEFT}
                ]
@@ -101,6 +97,9 @@ class BJParserFrame(tk.Frame):
             button.config(text=b['text'],command=getattr(self,b['name']+'Click'))
             button.pack(side=b['side'])
             setattr(self,'Button'+b['name'],button)
+        
+        self.master.bind('<Left>', self.KeepFileClick)
+        self.master.bind('<Right>', self.TossFileClick)
        
     def __createFileListBox(self):
         _h = int(self.winfo_height())
@@ -112,65 +111,42 @@ class BJParserFrame(tk.Frame):
                 font=font.Font(size=10,weight="bold"))
         self.FileListBoxFrameLabel.pack(side=tk.TOP,fill=tk.X)
         
-        
-#        self.style.configure('Keep.TFrame', background='blue')
-        self.KeepFileListBoxFrame = ttk.Frame(self.FileListBoxFrame, height = _h/3, width = _w/2)
-        self.KeepFileListBoxFrame.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
-        
-        KeepyScroll = ttk.Scrollbar(self.KeepFileListBoxFrame,orient=tk.VERTICAL)
-        KeepyScroll.pack(side=tk.LEFT,fill=tk.Y)
-        KeepxScroll = ttk.Scrollbar(self.KeepFileListBoxFrame, orient=tk.HORIZONTAL)
-        KeepxScroll.pack(side=tk.BOTTOM,fill=tk.X)
-        self.keepfilelist = tk.StringVar()
-        self.KeepFileListBox = tk.Listbox(self.KeepFileListBoxFrame, listvariable=self.keepfilelist, 
+        scrolls = {}
+        styles = {'Keep': (tk.LEFT, cl.LIGHTGREEN),
+                  'Toss': (tk.RIGHT, cl.LIGHTRED)}
+        for _prefix in ('Keep', 'Toss'):
+            setattr(self, _prefix+'FileListBoxFrame', ttk.Frame(self.FileListBoxFrame, height = _h/3, width = _w/2))   
+            getattr(self, _prefix+'FileListBoxFrame').pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
+            scrolls[_prefix+'yScroll']=ttk.Scrollbar(getattr(self, _prefix+'FileListBoxFrame'), orient=tk.VERTICAL)
+            scrolls[_prefix+'yScroll'].pack(side=styles[_prefix][0], fill=tk.Y)
+            scrolls[_prefix+'xScroll']=ttk.Scrollbar(getattr(self, _prefix+'FileListBoxFrame'), orient=tk.HORIZONTAL)
+            scrolls[_prefix+'xScroll'].pack(side=tk.BOTTOM, fill=tk.X)
+            setattr(self, _prefix+'filelist', tk.StringVar())
+                      
+            setattr(self, _prefix+'FileListBox', tk.Listbox(getattr(self, _prefix+'FileListBoxFrame'),
+                                                            listvariable=getattr(self, _prefix+'filelist'), 
                                       selectmode=tk.EXTENDED, 
                                       height = 20, width = 45, relief=tk.RAISED, bd=1,
-                                      bg=cl.LIGHTGREEN,
+                                      bg=styles[_prefix][1], 
                                       #font=Font(size=10),
-                                      xscrollcommand=KeepxScroll.set, 
-                                      yscrollcommand=KeepyScroll.set)
-        self.KeepFileListBox.bind('<<ListboxSelect>>', self.KeepListBoxClick)
-        KeepxScroll['command'] = self.KeepFileListBox.xview
-        KeepyScroll['command'] = self.KeepFileListBox.yview
-        self.KeepFileListBox.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
+                                      xscrollcommand=scrolls[_prefix+'xScroll'].set, 
+                                      yscrollcommand=scrolls[_prefix+'yScroll'].set))
+            
+            getattr(self, _prefix+'FileListBox').bind('<<ListboxSelect>>', getattr(self, _prefix+'ListBoxClick'))
+            scrolls[_prefix+'xScroll']['command'] = getattr(self, _prefix+'FileListBox').xview
+            scrolls[_prefix+'yScroll']['command'] = getattr(self, _prefix+'FileListBox').yview
+            getattr(self, _prefix+'FileListBox').pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
         
-        
-#        self.style.configure('Toss.TFrame', background='yellow')        
-        self.TossFileListBoxFrame = ttk.Frame(self.FileListBoxFrame, height = _h/3, width = _w/2)
-        self.TossFileListBoxFrame.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
-        
-        TossyScroll = ttk.Scrollbar(self.TossFileListBoxFrame,orient=tk.VERTICAL)
-        TossyScroll.pack(side=tk.RIGHT,fill=tk.Y)
-        TossxScroll = ttk.Scrollbar(self.TossFileListBoxFrame, orient=tk.HORIZONTAL)
-        TossxScroll.pack(side=tk.BOTTOM,fill=tk.X)
-        self.tossfilelist = tk.StringVar()
-        self.TossFileListBox = tk.Listbox(self.TossFileListBoxFrame, listvariable=self.tossfilelist, 
-                                      selectmode=tk.EXTENDED, 
-                                      height = 20, width = 45, relief=tk.RAISED, bd=1,
-                                      bg=cl.LIGHTRED,
-                                      #font=Font(size=10),
-                                      xscrollcommand=TossxScroll.set, 
-                                      yscrollcommand=TossyScroll.set)
-        self.TossFileListBox.bind('<<ListboxSelect>>', self.TossListBoxClick)
-        TossxScroll['command'] = self.TossFileListBox.xview
-        TossyScroll['command'] = self.TossFileListBox.yview
-        self.TossFileListBox.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
-        
-        self.UpdateFileListBoxFrameLabel()
       
     def __createCanvas(self):
         _h = int(self.winfo_height())
         _w = int(self.winfo_width())
-        print(self.winfo_geometry())
-        print(_h)
-        print(_w)
         self.KeepPlotCanvas = tk.Canvas(self.CanvasFrame, bg='green',
                                         height = _h/3, width = _w/2)
         self.KeepPlotCanvas.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
         self.TossPlotCanvas = tk.Canvas(self.CanvasFrame, bg='red',
                                         height = _h/3, width = _w/2)
         self.TossPlotCanvas.pack(side=tk.RIGHT, fill=tk.BOTH, expand=True)
-
 
         
     def ToFront(self):
@@ -184,52 +160,30 @@ class BJParserFrame(tk.Frame):
         self.Quit()
 
     def KeepListBoxClick(self, event):
+        self.__ListBoxClick('Keep')
 
-        selected = [self.KeepFileListBox.get(x) for x in self.KeepFileListBox.curselection()]
-        if selected == self.keep_selected_files:
-            self.logger.debug("Keep file selection unchanged.")
-            self.handler.flush()
-            return
-        if len(selected) > 1:
-            return
-        self.keep_selected_files = selected
-        _f = os.path.join(self.indir, selected[-1])
-        self.logger.debug("Got %s from keep list", _f)
-        self.handler.flush()
-        if os.path.exists(_f):
-            self.DisplayDataFigure('Keep', self.KeepPlotCanvas, _f)
 
     def TossListBoxClick(self, event):
+        self.__ListBoxClick('Toss')
 
-        selected = [self.TossFileListBox.get(x) for x in self.TossFileListBox.curselection()]
-        if selected == self.toss_selected_files:
-            self.logger.debug("Toss file selection unchanged.")
+
+    def __ListBoxClick(self, _prefix):
+        selected = [getattr(self, _prefix+'FileListBox').get(x) 
+                for x in getattr(self, _prefix+'FileListBox').curselection()]
+        if selected == getattr(self, _prefix+'_selected_files'):
+            self.logger.debug("%s file selection unchanged.", _prefix)
             self.handler.flush()
             return
         if len(selected) > 1:
             return
-        self.toss_selected_files = selected
+        setattr(self, _prefix+'_selected_files', selected)
         _f = os.path.join(self.indir, selected[-1])
-        self.logger.debug("Got %s from toss list", _f)
+        self.logger.debug("Got %s from %s list", _f, _prefix)
         self.handler.flush()
         if os.path.exists(_f):
-            self.DisplayDataFigure('Toss', self.TossPlotCanvas, _f)
+            self.DisplayDataFigure(_prefix, 
+                                   getattr(self, _prefix+'PlotCanvas'), _f)
             
-#    def ListBoxClick(self, event):
-#
-#        selected = [self.TossFileListBox.get(x) for x in self.TossFileListBox.curselection()]
-#        if selected == self.toss_selected_files:
-#            self.logger.debug("File selection unchanged.")
-#            self.handler.flush()
-#            return
-#        if len(selected) > 1:
-#            return
-#        self.toss_selected_files = selected
-#        _f = os.path.join(self.indir, selected[-1])
-#        self.logger.debug("Got %s from toss list", _f)
-#        self.handler.flush()
-#        if os.path.exists(_f):
-#            self.DisplayDataFigure('toss', self.TossPlotCanvas, _f)
 
     def ParseClick(self):
         self.checkOptions()
@@ -243,54 +197,69 @@ class BJParserFrame(tk.Frame):
         for _f in os.listdir(self.indir):
             if _f[-3:].lower() == self.opts.extension:
                 if os.path.isfile(os.path.join(self.indir,_f)):
-                    self.in_files.append(_f)
+                    self.Keep_files.append(_f)
             else:
                 self.logger.debug("Skipping %s", _f)
-        if len(self.in_files):    
+        if len(self.Keep_files):    
             self.updateKeepFileListBox()
             
 
     def updateKeepFileListBox(self):
-        self.keepfilelist.set(" ".join([x.replace(" ","_") for x in self.in_files]))
-#    def updateTossFileListBox(self):
-#        self.tossfilelist.set(" ".join([x.replace(" ","_") for x in self.in_files]))
-     
-    def RemoveFileClick(self):
-        self.checkOptions()
-        selected = [self.KeepFileListBox.get(x) for x in self.KeepFileListBox.curselection()]
-        todel = []
-        filelist = []
-        for i in range(0, len(self.in_files)):
-            for s in selected:
-                if self.in_files[i].replace(" ","_") == s:
-                    todel.append(i)
+        self.__updateFileListBox('Keep')
+
+    def updateTossFileListBox(self):
+        self.__updateFileListBox('Toss')
+
+    def __updateFileListBox(self, _prefix):
+            getattr(self, _prefix+'filelist').set(" ".join([x.replace(" ","_") 
+                for x in getattr(self, _prefix+'_files')]))    
+
+    def KeepFileClick(self, event=None):
+        self.__FileClick('Toss', 'Keep')
         
-        for i in range(0, len(self.in_files)):
-            if i not in todel:
-                       filelist.append(self.in_files[i])
-        self.in_files = filelist
-        self.updateFileListBox()
-        self.KeepFileListBox.selection_clear(0,tk.END)
-                
+    def TossFileClick(self, event=None):
+        self.__FileClick('Keep', 'Toss')
+  
+    def __FileClick(self, _from, _to):      
+        self.checkOptions()
+        selected = [getattr(self, _from+'FileListBox').get(x) 
+                    for x in getattr(self, _from+'FileListBox').curselection()]
+        tomove = []
+        filelist = []
+        for i in range(0, len(getattr(self, _from+'_files'))):
+            for s in selected:
+                if getattr(self, _from+'_files')[i].replace(" ","_") == s:
+                    tomove.append(i)
+        
+        for i in range(0, len(getattr(self, _from+'_files'))):
+            if i not in tomove:
+                filelist.append(getattr(self, _from+'_files')[i])
+            else:
+                getattr(self, _to+'_files').append(getattr(self, _from+'_files')[i])
+        setattr(self, _from+'_files', filelist)
+        getattr(self, _from+'FileListBox').selection_clear(0,tk.END)       
+        self.__updateFileListBox(_to)
+        self.__updateFileListBox(_from)
+        
     def SpawnOutputDialogClick(self):
         outdir = tk.filedialog.askdirectory(title="Select Output Directory", initialdir=self.outdir)
         if os.path.exists(outdir):
             self.outdir = outdir
             self.UpdateFileListBoxFrameLabel()
 
-    def UpdateFileListBoxFrameLabel(self):
-        self.FileListBoxFrameLabelVar.set("Output to: %s"% (self.outdir) )
-
     
     def Quit(self):
         self.master.destroy()
 
-    def checkOptions(self):
-        if self.cache['last_input_path'] == None:
-            self.cache['last_input_path'] = os.environ['HOME']
+    def checkOptions(self):       
+        if not self.cache['last_input_path']:
+            self.cache['last_input_path'] = os.getcwd()
         if not os.path.exists(self.cache['last_input_path']):
-            self.cache['last_input_path'] = os.environ['HOME']
-        print("Nothing Yet.")
+            self.last_input_path = os.path.expanduser('~')
+        if not self.outdir:
+            self.outdir = os.path.join(self.indir, 'parsed')
+        self.FileListBoxFrameLabelVar.set("Output to: %s"% (self.outdir) )
+        
 
     def Parse(self):
         print("Nothing here yet")
@@ -310,7 +279,6 @@ class BJParserFrame(tk.Frame):
                 distanceData.append(float(liness[0]))
                 currentData.append(float(liness[1]))
 
-        #https://matplotlib.org/gallery/user_interfaces/embedding_in_tk_canvas_sgskip.html
         fig = mpl.figure.Figure(figsize=(5, 4), dpi=80)
         ax = fig.add_subplot(111)
         ax.plot(distanceData, currentData)
@@ -319,13 +287,9 @@ class BJParserFrame(tk.Frame):
         figure_canvas_agg.draw()
         figure_x, figure_y, figure_w, figure_h = fig.bbox.bounds
         figure_w, figure_h = int(figure_w), int(figure_h)
-        #self.fig_photo = tk.PhotoImage(master=master, width=figure_w, height=figure_h)
+        #https://matplotlib.org/gallery/user_interfaces/embedding_in_tk_canvas_sgskip.html
         setattr(self, label+'fig_photo', tk.PhotoImage(master=master, width=figure_w, height=figure_h))
-    
-        # Position: convert from top-left anchor to center anchor
         getattr(self, label+'PlotCanvas').create_image(figure_w/2, 
-               figure_h/2, image=getattr(self, label+'fig_photo'))
-        #self.PlotCanvas.create_image(figure_w/2, figure_h/2, image=self.fig_photo)
-    
+               figure_h/2, image=getattr(self, label+'fig_photo'))    
         # Unfortunately, there's no accessor for the pointer to the native renderer
         tkagg.blit(getattr(self, label+'fig_photo'), figure_canvas_agg.get_renderer()._renderer, colormode=2)
